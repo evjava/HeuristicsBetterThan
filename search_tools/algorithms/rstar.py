21640021bdf98b2d1154b970c353701045c40569
imbirik <32593555+imbirik@users.noreply.github.com>
2021-12-13 01:53:57 +0300

    added rstar.py

--- search_tools/algorithms/rstar.py
+++ search_tools/algorithms/rstar.py
@@ -1,6 +1,8 @@
 from algorithms.search_algorithm import SearchAlgorithm
 from node import Node
-from containers import Open, Closed
+from containers3 import Open, Closed
+from containers import Open as Open1
+from containers import Closed as Closed1
 from area import Area
 from algorithms.astar import Astar
 
@@ -10,72 +12,64 @@ INF = 1e12
 class Rstar(SearchAlgorithm):
     def __init__(self, heuristic, D, K, w):
         super().__init__(f'A*(h={heuristic.__name__})')
-        self.heuristic = heuristic
-        self.D = D
-        self.K = K
-        self.w
-        self.possible_bp = {}
-
+        self.h = heuristic
+        self.D, self.K, self.w = D, K, w
+        self.astar = Bounded_Astar(h=heuristic.__name__)
 
     def run(self, area: Area, start_coord, goal_coord):
+        path, g, k, bp, avoid = {}, {}, {}, {}, {}
 
-        start = Node(coord=start_coord)
-        goal = Node(coord=goal_coord)
+        start, goal = start_coord, goal_coord
+        open, closed = Open(), Closed()
 
-        calc_h = lambda c: self.heuristic(*c, *goal_coord)
-
-        op_nodes = Open()
-        op_nodes.push(start)
-        cl_nodes = Closed()
-
-        astar = Bounded_Astar(h=self.heuristic.__name__)
+        open.push(start)
 
         def update(s):
-            if s.g > self.w * self.heuristic(s.coord, self.start) or (s.path_to_bp is None and s.avoid):
-                op_nodes.push(s, priority=(1, s.g+self.w*self.h(s.coord, start.coord)))
+            if g[s] > self.w * self.heuristic(s, start) or (
+                    path[s][bp[s]][0] is None and avoid[s]):
+                k[s] = (1, g[s] + self.w * self.h(s, start))
             else:
-                op_nodes.push(s, priority=(0, s.g + self.w * self.h(s.coord, start.coord)))
+                k[s] = (0, g[s] + self.w * self.h(s, start))
+            open.push(s, key=k[s])
 
         def reevaluate(s):
-            s.path_to_bp, clow = astar(area, s.bp, s, 150)
-            s.g = clow + s.bp.g
+            path[s][bp[s]] = self.astar(area, bp[s], s, 150)
 
-            if s.path_to_bp is None or (s.g > self.w*self.heuristic(start.coord, s.coord)):
-                s.avoid = True
-                for s_new, clow_new in self.possible_bp[s]:
-                    if s_new.g + clow_new < s.g:
-                        s.g = s_new.g + clow_new
-                        s.bp = s_new
+            if path[s][bp[s]] is None or (g[s] > self.w * self.h(start, s)):
+                avoid[s] = True
+                bp[s] = min(path[s].keys(), key=lambda bp_new: g[bp_new] + path[s][bp_new][1])
+
+            g[s] = path[s][bp[s]][1] + g[bp[s]]
 
             update(s)
 
-        while not op_nodes.is_empty:
-            s = op_nodes.pop_best()
-            if op_nodes.priorities[goal] < op_nodes.priorities[s]:
+        while not open.is_empty:
+            s = open.pop_best()
+            if k[goal] < k[s]:
                 break
-            if s != start and s.path_to_bp is None:
+
+            if s != start and path[s][bp[s]] is None:
                 reevaluate(s)
             else:
-                cl_nodes.push(s)
-                succs = area.random_neighbors(s.coord, self.K, self.D)
-                if area.dist(s.coord, goal.coord) < self.D:
-                    succs.append(goal.coord)
-                succs = [coord for coord in succs if not cl_nodes.is_visited(coord)]
-                for s_new_coord in succs:
-                    path, clow = None, self.heuristic(s.coord, s_new_coord)
+                closed.push(s)
+                succs = area.random_neighbors(s, self.K, self.D)
+                if area.dist(s, goal) <= self.D:
+                    succs.append(goal)
+                succs = [coord for coord in succs if not closed.is_visited(coord)]
+                for s_new in succs:
 
-                    if op_nodes.find_by_coord(s_new_coord) is None:
-                        s_new = Node(s_new_coord)
-                        s_new.g = INF
-                        s_new.bp = None
-                    else:
-                        s_new = op_nodes.find_by_coord(s_new_coord)
-                    if s_new.bp is None or s.g+clow < s_new.g:
-                        s_new.g = s.g+clow
-                        s_new.bp = s
+                    if open.find(s_new) is None:
+                        g[s_new], bp[s_new] = INF, None
+                        path[s] = None
+
+                    path[s][s_new] = None, self.h(s, s_new)
+                    clow = path[s][s_new][1]
+
+                    if bp[s_new] is None or g[s]+clow < s_new.g:
+                        g[s_new], bp[s_new] = g[s]+clow, s
                         update(s_new)
 
-        return op_nodes.find_by_coord(goal_coord), op_nodes, cl_nodes
+        return goal, open, closed
 
 
 class Bounded_Astar(SearchAlgorithm):
@@ -97,9 +91,9 @@ class Bounded_Astar(SearchAlgorithm):
 
         calc_h = lambda c: self.heuristic(*c, *goal_coord)
 
-        op_nodes = Open()
+        op_nodes = Open1()
         op_nodes.push(start)
-        cl_nodes = Closed()
+        cl_nodes = Closed1()
 
         while not op_nodes.is_empty:
             if len(cl_nodes) > max_closed:
--- search_tools/containers3.py
+++ search_tools/containers3.py
@@ -1,18 +1,16 @@
 from node import Node
 from queue import PriorityQueue
 
+
 class Open:
-    def __init__(self): 
+    def __init__(self):
         self.elements = {}
         self.queue = PriorityQueue()
 
-    def push(self, node: Node, priority=None): 
-        self.elements[node.coord] = node
+    def push(self, coord, key):
+        self.elements[coord] = coord
+        self.queue.put((key, coord))
 
-        key = priority if priority is not None else node.comp()
-
-        self.queue.put((key, node))
-        
     def __iter__(self):
         return iter(self.elements.values())
 
@@ -25,26 +23,23 @@ class Open:
 
     @property
     def is_not_empty(self):
-        return not(self.is_empty)
+        return not (self.is_empty)
 
-    def find_by_coord(self, coord):
+    def find(self, coord):
         return self.elements.get(coord)
-
-    def find(self, node):
-        return self.find_by_coord(node.coord)
 
     def peek_best(self):
         return self.queue.queue[0][1]
 
     def pop_best(self):
         while not self.is_empty:
-            node = self.queue.get()[1]
-            if node.coord in self.elements:
-                del self.elements[node.coord]
+            coord = self.queue.get()[1]
+            if coord in self.elements:
+                del self.elements[coord]
                 # todo eliminate, move to some context? Maybe area?
-                node.time = Node.TIME
-                Node.TIME += 1
-                return node
+                #node.time = Node.TIME
+                #Node.TIME += 1
+                return coord
         return None
 
 
@@ -62,16 +57,16 @@ class Closed:
     def is_empty(self):
         return len(self) == 0
 
-    def find(self, node):
-        return self.elements.get(node.coord)
+    def find(self, coord):
+        return self.elements.get(coord)
 
     def is_visited(self, coord):
         return coord in self.elements
-    
-    def push(self, node): 
-        if self.is_visited(node.coord):
+
+    def push(self, coord):
+        if self.is_visited(coord):
             return False
         else:
-            self.elements[node.coord] = node
+            self.elements[coord] = coord
             return True
-    
+
